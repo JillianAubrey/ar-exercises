767075ff476cba5d393aca08f2c7267795c257d6
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-11-15 07:57:08 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -11,9 +11,10 @@ puts "----------"
 
 # Your code goes here ...
 
-Store.create!(
-  name: "Test", 
-  annual_revenue: 300000, 
-  mens_apparel: false, 
-  womens_apparel: false
-)
+puts "Provide a name for a new store:"
+@new_name = gets.chomp
+@new_store = Store.create(name: @new_name)
+
+@new_store.errors.messages.each do |msg|
+  puts "#{msg[0]} #{msg[1][0]}"
+end
